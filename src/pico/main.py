48d1c06f42b65c6fda21d5e55b3582e7291828eb
thjaed <thomasjedwards10@gmail.com>
2025-05-17 14:00:14 +0100

    improved efficiency and scrolling logic

--- src/pico/main.py
+++ src/pico/main.py
@@ -9,65 +9,81 @@ from ui import UI
 
 ui = UI()
 ui.clear()
+fps = 30
+sleep_time = int((1/fps) * 1000)
+
+PINS = [12, 13, 14, 15]
+buttons = []
 
 def press_handler(btn, pattern):
-    if pattern == Button.SINGLE_PRESS and btn.get_id() == 15:
-        ui.scroll += -50
-    elif pattern == Button.SINGLE_PRESS and btn.get_id() == 14:
-        ui.scroll += 50
-        
-# Pin numbers for your buttons
-PINS = [12, 13, 14, 15]
-
-# Create buttons
-buttons = []
+    if pattern == Button.SINGLE_PRESS and btn.get_id() == 14: # Button X
+        ui.scroll_events(direction="up")
+        update_events_page()
+    elif pattern == Button.SINGLE_PRESS and btn.get_id() == 15: # Button Y
+        ui.scroll_events(direction="down")
+        update_events_page()
 
 for pin in PINS:
     btn = Button(Button.MODE_DIGITAL, pin)
 
     btn.on_press(press_handler) \
-       .on_press_for(press_handler, 700)  # 1000ms = long press
-       
+       .on_press_for(press_handler, 700)  # 700ms = long press
     buttons.append(btn)
 
 async def monitor_buttons():
     while True:
         for btn in buttons:
             btn.read()
-        await asyncio.sleep_ms(10)
+        await asyncio.sleep_ms(10) # 10ms poll time
         
-async def main():
-    asyncio.create_task(monitor_buttons())
-    
-    while True:
+def update_menu_bar():
         time_secs = utime.time()
 
         clock = calendar.get_clock(time_secs)
         date = calendar.get_date(time_secs)
         battery_level = battery.percentage()
-
         
-
-        with open("cal_today.jsonl", "r") as f:
-            ui.clear_event_area()
-            for index, line in enumerate(f):
-                event = ujson.loads(line)
-                
-                ui.draw_event_box(
-                    index=index,
-                    title=event.get("title"),
-                    time=f"{calendar.get_clock(event.get('start'))} - {calendar.get_clock(event.get('end'))}",
-                    location=event.get("location"),
-                    attendees="Attendees Placeholder"
-                    )     
-                
         ui.draw_menu_bar(clock=clock,
                          date=date,
                          battery_level=battery_level,
                          charging=battery.charging()
                          )
 
-        ui.update()
-        await asyncio.sleep_ms(33)
+def update_events_page():
+    ui.clear_event_area()
+    
+    with open("cal_today.jsonl", "r") as f:
+        total_content_height = 0
+        ui.event_heights = []
+        for line in f:
+            event = ujson.loads(line)
+ 
+            ui.draw_event_box(
+                total_content_height=total_content_height,
+                title=event.get("title"),
+                time=f"{calendar.get_clock(event.get('start'))} - {calendar.get_clock(event.get('end'))}",
+                location=event.get("location"),
+                attendees=event.get("attendees")
+            )
+            
+            total_content_height += ui.box_height
+            
+    
+    update_menu_bar()
 
+async def update_menu_bar_loop():
+    while True:
+        update_menu_bar()
+        await asyncio.sleep_ms(1000)
+         
+async def main():
+    asyncio.create_task(monitor_buttons())
+    asyncio.create_task(update_menu_bar_loop())
+    
+    while True:
+        ui.update()
+        await asyncio.sleep_ms(sleep_time)
+        
+update_events_page()
 asyncio.run(main())
+
--- src/pico/ui.py
+++ src/pico/ui.py
@@ -11,12 +11,51 @@ class UI:
         self.BLACK = self.display.create_pen(0, 0, 0)
         self.GREEN = self.display.create_pen(0, 255, 0)   
         
-        self.scroll = 0
-    
+        self.context = "home"
+        self.scroll_distance = 0
+        self.box_height = 0
+        self.event_heights = []
+        
     def clear(self):
         self.display.set_backlight(0.8)
         self.display.set_pen(self.BLACK)
         self.display.clear()
+
+    def clear_event_area(self):
+        self.display.set_pen(self.BLACK)
+        self.display.rectangle(0, 16, 320, 224)
+
+    def update(self):
+        self.display.update()
+    
+    def scroll_events(self, direction):
+        visible_height = 225
+        scroll_offset = -self.scroll_distance
+        curr_height = 0
+
+        if direction == "down":
+            for box_height in self.event_heights:
+                box_top = curr_height
+                box_bottom = curr_height + box_height
+
+                if box_bottom > scroll_offset + visible_height:
+                    self.scroll_distance = -(box_bottom - visible_height)
+                    return
+
+                curr_height += box_height
+
+        elif direction == "up":
+            for index, box_height in enumerate(self.event_heights):
+                box_top = curr_height
+
+                if box_top >= scroll_offset:
+                    if index > 0:
+                        self.scroll_distance = -sum(self.event_heights[:index - 1])
+                    else:
+                        self.scroll_distance = 0
+                    return
+
+                curr_height += box_height
         
     def draw_menu_bar(self, clock, date, battery_level, charging):
         clock_width = self.display.measure_text(clock, scale=2)
@@ -45,22 +84,39 @@ class UI:
 
         self.display.rectangle(293, 5, (round((battery_level / 100) * 19)), 4) # Charge level
     
-    def draw_event_box(self, index, title, time, location, attendees):
-            y_offset = 16 + (index * 75) + self.scroll
-
+    def draw_event_box(self, total_content_height, title, time, location, attendees):
+            self.total_content_height = total_content_height
+            start_y = 16 + self.scroll_distance + total_content_height
+            line_y = start_y
+            
+            self.box_height = 0
+            
+            if title: self.box_height += 20
+            if time: self.box_height += 14
+            if location: self.box_height += 14
+            if attendees: self.box_height += 14
+            
+            self.box_height += 2
+            self.event_heights.append(self.box_height)
+            
             self.display.set_pen(self.GREY)
-            self.display.rectangle(0, y_offset, 320, 75) # Event box
+            self.display.rectangle(0, start_y, 320, self.box_height)
             self.display.set_pen(self.WHITE)
-            self.display.line(0, y_offset + 74, 320, y_offset + 74) # Bottom bar
+            self.display.line(0, self.box_height + line_y - 1, 320, self.box_height + line_y - 1) # Bottom bar
+            
+            self.display.set_pen(self.WHITE)
+            
+            if title:
+                self.display.text(title, 5, line_y, scale=3)
+                line_y += 20
+            if time:
+                self.display.text(time, 5, line_y, scale=2)
+                line_y += 14
+            if location:
+                self.display.text(location, 5, line_y, scale=2)
+                line_y += 14    
+            if attendees:
+                self.display.text(attendees, 5, line_y, scale=2)
+                line_y += 14
+                
 
-            self.display.text(title, 5, y_offset + 2, scale=3) # Title
-            self.display.text(time, 5, y_offset + 27, scale=2) # Time
-            self.display.text(location, 5, y_offset + 42, scale=2) # Location
-            self.display.text(attendees, 5, y_offset + 57, scale=2) # Attendees
-    
-    def update(self):
-        self.display.update()
-    
-    def clear_event_area(self):
-        self.display.set_pen(self.BLACK)
-        self.display.rectangle(0, 16, 320, 224)
